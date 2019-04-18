f453b548af7b5e0ae23f2a985358e9b65df383f8
Colin Smith <smith.colin00@gmail.com>
2019-04-18 15:53:29 -0600

    add/implement selectable

---
+++ pt-proto-1/Assets/Selectable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class Selectable : MonoBehaviour
+{
+    SelectionManager manager;
+    protected bool selected;
+
+    void Start()
+    {
+        manager = SelectionManager.instance;
+    }
+
+    void OnMouseDown()
+    {
+        OnSelect();
+        manager.OnSelect(this);
+    }
+
+    protected virtual void OnSelect()
+    {
+        selected = true;
+    }
+    public virtual void OnDeselect()
+    {
+        selected = false;
+    }
+}
