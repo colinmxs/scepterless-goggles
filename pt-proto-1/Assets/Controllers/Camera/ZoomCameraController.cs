6ab8c61105be18b23c3800669bab024c56f7fe76
Colin Smith <smith.colin00@gmail.com>
2019-04-18 15:54:50 -0600

    add/implement zoomcameracontroller

---
+++ pt-proto-1/Assets/Controllers/Camera/ZoomCameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ZoomCameraController : MonoBehaviour
+{
+    public float stepSize = 0.5f;
+    public float maxZoom;
+    public float minZoom;
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        {
+            Camera.main.orthographicSize -= stepSize;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        {
+            Camera.main.orthographicSize += stepSize;
+        }
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom, maxZoom);
+    }
+}
