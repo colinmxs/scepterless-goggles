88d581fe2dc875fb21d360a166035bace1966df0
Colin Smith <smith.colin00@gmail.com>
2019-04-29 21:09:46 -0600

    add CharacterSelectable

---
+++ pt-proto-1/Assets/CharacterSelectable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterSelectable : Selectable
+{
+    private Canvas indicatorCanvas;
+    
+    void Awake()
+    {
+        indicatorCanvas = GetComponentInChildren<Canvas>();
+        indicatorCanvas.enabled = false;
+    }
+    
+    public override void OnSelect()
+    {
+        Debug.Log("CharacterSelectable::OnSelect::");
+        base.OnSelect();
+        indicatorCanvas.enabled = true;
+    }
+
+    public override void OnDeselect()
+    {
+        base.OnDeselect();
+        indicatorCanvas.enabled = false;
+    }
+}
