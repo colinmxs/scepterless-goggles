0cebdf768b972a09c01a831b70a3654efe0a11d2
Colin Smith <smith.colin00@gmail.com>
2019-04-29 21:09:25 -0600

    add Moveable and meta

---
+++ pt-proto-1/Assets/Moveable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Moveable : MonoBehaviour
+{
+
+    Transform _transform;
+    Vector3 destination;
+    float speed;
+
+    void Start()
+    {
+        _transform = transform;
+    }
+
+    public void MoveAsync(Vector3 destination, float speed)
+    {
+        this.destination = destination;
+        this.speed = speed;
+    }
+
+    void FixedUpdate()
+    {
+        float step = (speed / (_transform.position - destination).magnitude) * Time.fixedDeltaTime;
+        transform.position = Vector3.Lerp(_transform.position, destination, step);
+    }
+}
