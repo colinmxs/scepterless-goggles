e32d4f68d7e0067c8bacd1236fe7899621d28838
Colin Smith <smith.colin00@gmail.com>
2019-04-29 21:09:59 -0600

    add CharacterController

---
+++ pt-proto-1/Assets/Controllers/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    Selectable selectable;
+    Moveable moveable;
+    SelectionManager manager;
+
+    void Start()
+    {        
+        selectable = GetComponent<Selectable>();
+        moveable = GetComponent<Moveable>();
+        manager = SelectionManager.instance;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("CharacterController::OnMouseDown::");
+        selectable.OnSelect();
+        manager.OnSelect(this);
+    }
+
+    public void Deselect()
+    {
+        selectable.OnDeselect();
+    }
+
+    public void Move(Vector3 destination)
+    {
+        moveable.MoveAsync(destination, speed);
+    }
+}
