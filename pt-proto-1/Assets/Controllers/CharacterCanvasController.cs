7dea79415c69854d3486f79a9fb0745d435c7bd3
Colin Smith <ColinSmith1@boisestate.edu>
2019-04-30 21:14:17 -0600

    add CharacterCanvasController

---
+++ pt-proto-1/Assets/Controllers/CharacterCanvasController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCanvasController : MonoBehaviour
+{
+    public Image dingy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
