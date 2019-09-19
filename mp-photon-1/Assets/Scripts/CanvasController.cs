534ebe1eede0d72c67415450e434c6bfd07509f8
Colin Smith <ColinSmith1@boisestate.edu>
2019-09-18 18:30:50 -0600

    sync

--- mp-photon-1/Assets/Scripts/CanvasController.cs
+++ mp-photon-1/Assets/Scripts/CanvasController.cs
@@ -3,39 +3,86 @@ using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
-    Text myText;
+    Text text;
     int score = 0;
 
     void Start()
     {
-        gameObject.AddComponent<Canvas>();
-        var canvas = gameObject.GetComponent<Canvas>();
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-        gameObject.AddComponent<CanvasScaler>();
-        gameObject.AddComponent<GraphicRaycaster>();
+        GameObject myGO;
+        GameObject myText;
+        Canvas myCanvas;
+        Text text;
+        RectTransform rectTransform;
+
+        // Canvas
+        myGO = new GameObject();
+        myGO.name = "TestCanvas";
+        myGO.AddComponent<Canvas>();
+
+        myCanvas = myGO.GetComponent<Canvas>();
+        myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        myGO.AddComponent<CanvasScaler>();
+        myGO.AddComponent<GraphicRaycaster>();
 
         // Text
-        var myScore = new GameObject();
-        myScore.transform.parent = gameObject.transform;
-        myScore.name = "Score";
+        myText = new GameObject();
+        myText.transform.parent = myGO.transform;
+        myText.name = "wibble";
 
-        myText = myScore.AddComponent<Text>();
-        myText.font = Font.CreateDynamicFontFromOSFont("Arial", 100);
-        myText.text = "Score: " + score;
-        myText.fontSize = 100;
+        text = myText.AddComponent<Text>();
+        text.font = Font.CreateDynamicFontFromOSFont("Arial", 14);
+        text.text = "wobble";
+        text.fontSize = 100;
 
         // Text position
-        var rectTransform = myText.GetComponent<RectTransform>();
-        rectTransform.localPosition = new Vector3(0, 0, 0);
-        rectTransform.sizeDelta = new Vector2(450, 200);
-    }    
+        //rectTransform = text.GetComponent<RectTransform>();
+        //rectTransform.localPosition = new Vector3(0, 0, 0);
+        //rectTransform.sizeDelta = new Vector2(1000, 200);
+    }
+
+    //void Start()
+    //{
+    //    GameObject scoreUI;
+    //    GameObject myText;
+    //    Canvas scoreCanvas;        
+    //    RectTransform rectTransform;
+
+    //    // Canvas
+    //    scoreUI = new GameObject
+    //    {
+    //        name = "Score Canvas"
+    //    };
+
+    //    scoreCanvas = scoreUI.AddComponent<Canvas>();
+    //    scoreCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+    //    scoreUI.AddComponent<CanvasScaler>();
+    //    scoreUI.AddComponent<GraphicRaycaster>();
+
+    //    // Text
+    //    myText = new GameObject();
+    //    myText.transform.parent = scoreUI.transform;
+    //    myText.name = "Player Score";
+
+    //    text = myText.AddComponent<Text>();
+    //    text.font = Font.CreateDynamicFontFromOSFont("Arial", 14);
+    //    text.text = "Score: " + score;
+    //    text.fontSize = 100;
+
+    //    // Text position
+    //    rectTransform = text.GetComponent<RectTransform>();
+    //    //rectTransform.anchorMin = new Vector2(0, 1);
+    //    //rectTransform.anchorMax = new Vector2(0, 1);
+    //    //rectTransform.pivot = new Vector2(0, 1);
+    //    rectTransform.localPosition = new Vector3(0, 0, 0);
+    //    rectTransform.sizeDelta = new Vector2(1000, 200);
+    //}    
     public void IncrementScore()
     {
-        score += 1;
-        myText.text = "Score: " + score;
+        //score += 1;
+        //text.text = "Score: " + score;
     }
     public void DisplayWinMessage()
     {
-        myText.text = "You WIN MOTHERFUCKER";
+        //text.text = "You WIN MOTHERFUCKER";
     }
 }
